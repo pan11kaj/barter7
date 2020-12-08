a6998159e502da5d7ab5a0438e40af50413b4cba
pan11kaj <68064022+pan11kaj@users.noreply.github.com>
2020-12-08 10:32:00 +0530

    Add files via upload

---
+++ compo/appdrawerNavigator.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import Drawer from '../compo/drawer';
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import {BottomTab} from '../compo/tab';
+import Update from '../Screens/setting';
+import MyExchenges from '../Screens/Mybarter'
+
+export const SideDrawer = createDrawerNavigator({
+  Home:{screen:BottomTab},
+  Settings:{screen:Update},
+  MyBarters:{screen:MyExchenges}
+}
+,
+  {
+      contentComponent:Drawer
+  }
+  ,{
+      initialRouteName:'Home'
+  }
+)
+export default SideDrawer;
